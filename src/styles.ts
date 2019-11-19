fca16f9d6d3c2eceea9e9d5e7adedd414003f7d2
Jameyel "Jay" Johnson <jameyel@everlywell.com>
2019-11-19 16:06:32 -0600

    add z indicator toggle

--- src/styles.ts
+++ src/styles.ts
@@ -41,7 +41,7 @@ const VebugWrapper = styled.div`
     font-weight: bold;
     box-shadow: 0 0.25em 0.25em -0.125em rgba(0, 0, 0, 0.25),
       0 0.5em 1.25em rgba(0, 0, 0, 0.5);
-    z-index: 9999;
+    z-index: 99999;
   }
 `;
 
